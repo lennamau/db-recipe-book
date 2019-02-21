cb0fa2963b28bcb79b38d05abec3e9a2eff82f3d
Lenna <lenna@lennamau.com>
2019-02-21 15:32:34 -0700

    migrations and seeds setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express= require('express')
+const knex = require('knex')
+const knexConfig = require('./knexfile.js')
+
+const server = express();
+
+const db = knex(knexConfig.development)
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send('Cookbook API')
+});
+const port = 4000;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
